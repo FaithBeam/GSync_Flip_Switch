5dec6565d9ec70cefda178812a3b0fed5133543f
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2017-10-03 21:57:03 -0400

    Initial Commit

---
+++ GSync_Flip_Switch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+determine_gsync_state() {
+    CURRENTMETAMODE="$(nvidia-settings -q CurrentMetaMode)"
+    
+    if [[ "${CURRENTMETAMODE}" == *"AllowGSYNC=Off"* ]]; then
+        GSYNCSTATE="OFF"
+    else
+        GSYNCSTATE="ON"
+    fi
+}
+
+flip_gsync_mode() {
+    CURRENTMETAMODE="${CURRENTMETAMODE##*:: }"
+    NVIDIACOMMAND="nvidia-settings --assign CurrentMetaMode=\""
+
+    if [[ "${GSYNCSTATE}" == "OFF" ]]; then
+        NVIDIACOMMAND+=${CURRENTMETAMODE//{AllowGSYNC=Off,/{AllowGSYNC=On, }
+    else
+        NVIDIACOMMAND+=${CURRENTMETAMODE//{/{AllowGSYNC=Off, }
+    fi
+
+    NVIDIACOMMAND+='"'
+    eval "$NVIDIACOMMAND"
+}
+
+main() {
+    determine_gsync_state
+    flip_gsync_mode
+}
+
+main
